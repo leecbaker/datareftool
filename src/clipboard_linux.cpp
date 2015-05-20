049a1141923c0939e10fe5fa3ab9ba7587e841a0
sparker256 <bgood@velocity.net>
2015-05-19 20:42:33 -0400

    Added setClipboard function for Linux using xclip

--- src/clipboard_linux.cpp
+++ src/clipboard_linux.cpp
@@ -6,5 +6,13 @@ std::string getClipboard() {
 }
 
 void setClipboard(const std::string & s) {
-	//TODO
+    // Use xclip
+    // TODO(sparker256): Need to check if xclip is on system
+    // If not give method to install it
+    // sudo apt-get install xclip
+    std::string s1 = "echo ";
+    std::string s2 = " | xclip -sel c";
+    std::string s3 = s1+ s +s2;
+    system(s3.c_str());
+
 }
