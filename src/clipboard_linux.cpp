cfd66f4f82b5b03bcbae8f4af25b5c1e1372f27b
sparker256 <bgood@velocity.net>
2015-05-21 16:58:17 -0400

    Have paste for Linux working correctly using xclip

--- src/clipboard_linux.cpp
+++ src/clipboard_linux.cpp
@@ -5,31 +5,23 @@
 #include <fstream>
 #include <streambuf>
 
+#include <iostream>
+#include <cstdio>
+
 // implementation from @sparker256
 std::string getClipboard() {
 
-    std::string command = "xclip -o -sel > ./Resources/plugins/DataRefTool/linxclip.txt";
-
-    if(0 != system(command.c_str())) {
+    FILE *res = popen("xclip -o -sel", "r");
+    if (!res) {
         XPLMDebugString("DRT: Paste command failed. Do you have xclip on your system?\n");
     }
-
-    // TODO @sparker256
-    // Find a way to get the stdout from "xclip -o -sel" into std::string result
-
-    // This is a hack using a txt file to store selection and read it back into result
-    // When I get the stdout to std::string result working I will use that.
-
-    std::ifstream t("./Resources/plugins/DataRefTool/linxclip.txt");
+    char buffer[1024];
     std::string result;
 
-    t.seekg(0, std::ios::end);
-    result.reserve(t.tellg());
-    t.seekg(0, std::ios::beg);
-
-    result.assign((std::istreambuf_iterator<char>(t)),
-                std::istreambuf_iterator<char>());
-
+    while(fgets(buffer, sizeof(buffer), res)){
+        result += buffer;
+    }
+    pclose(res);
     return result;
 }
 
