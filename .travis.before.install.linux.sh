87f1532e0d5e9981828d710e5fdc9205d6898e58
Lee Baker <lee@leecbaker.com>
2016-01-09 00:57:30 -0600

    Travis Use apt add-on to simplify scripts

--- .travis.before.install.linux.sh
+++
@@ -1,5 +0,0 @@
- #!/bin/bash
- sudo apt-get update
- sudo apt-get install -y cmake
- sudo apt-get install -y libboost1.54-dev libboost-filesystem1.54-dev libboost-iostreams1.54-dev libboost-system1.54-dev
- sudo apt-get install -y libx11-dev # libboost-all-dev
