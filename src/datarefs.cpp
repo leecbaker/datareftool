c242203ef2a2754c54faa1e9b491cbd1ee7d2801
Lee Baker <lee@leecbaker.com>
2016-01-09 00:06:11 -0600

    Add a const, because why not...

--- src/datarefs.cpp
+++ src/datarefs.cpp
@@ -219,7 +219,7 @@ void doDatarefSearch(const std::string & search_term, bool regex, bool case_inse
 	// Feels a bit messy using all these lambdas, and also I'm a bit skeptical if it all getting optimized well
 	const auto string_search = [case_insensitive](const std::string & haystack, const std::vector<std::string> & needles) -> bool {
 
-		const auto case_insensitive_comparator = [](char ch1, char ch2) -> bool { return ::toupper(ch1) == ::toupper(ch2); };
+		const auto case_insensitive_comparator = [](const char ch1, const char ch2) -> bool { return ::toupper(ch1) == ::toupper(ch2); };
 
 		for(const std::string & needle : needles) {
 			std::string::const_iterator it;
