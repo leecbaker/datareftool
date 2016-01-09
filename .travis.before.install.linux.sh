a68dc7474613c67996efea56072eee89b0ddbacc
Lee Baker <lee@leecbaker.com>
2016-01-09 00:28:00 -0600

    On linux, update the system g++ / clang++

--- .travis.before.install.linux.sh
+++ .travis.before.install.linux.sh
@@ -7,12 +7,12 @@
  sudo apt-get install cmake
  if [ "$CXX" = "g++" ]; then sudo apt-get install gcc-4.9 g++-4.9; fi
  if [ "$CXX" = "clang++" ]; then sudo apt-get install clang-3.6; fi
+ sudo update-alternatives --set gcc `which gcc-4.9`
+ sudo update-alternatives --set g++ `which g++-4.9`
+ sudo update-alternatives --set clang `which clang-3.6`
+ sudo update-alternatives --set clang++ `which clang++-3.6`
  sudo apt-get install libboost1.54-dev libboost-filesystem1.54-dev libboost-iostreams1.54-dev libboost-system1.54-dev
  sudo apt-get install libx11-dev # libboost-all-dev
- if [ "$CC" = "gcc" ]; then CC=gcc-4.9; fi
- if [ "$CC" = "clang" ]; then CC=clang-3.6; fi
- if [ "$CXX" = "g++" ]; then CXX=g++-4.9; fi
- if [ "$CXX" = "clang++" ]; then CXX=clang++-3.6; fi
  cmake --version
  $CC --version
  $CXX --version
