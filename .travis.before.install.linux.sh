516d6de9a64008daee092ad11fe553a003d642a1
Lee Baker <lee@leecbaker.com>
2016-01-09 00:38:35 -0600

    Put some debug code in the Travis linux build

--- .travis.before.install.linux.sh
+++ .travis.before.install.linux.sh
@@ -6,11 +6,17 @@
  sudo apt-get install
  sudo apt-get install cmake
  if [ "$CXX" = "g++" ]; then sudo apt-get install gcc-4.9 g++-4.9; fi
- if [ "$CXX" = "clang++" ]; then sudo apt-get install clang-3.6; fi
- sudo update-alternatives --set gcc `which gcc-4.9`
- sudo update-alternatives --set g++ `which g++-4.9`
- sudo update-alternatives --set clang `which clang-3.6`
- sudo update-alternatives --set clang++ `which clang++-3.6`
+ if [ "$CXX" = "clang++" ]; then sudo apt-get install clang-3.6 clang++-3.6; fi
+
+ which clang++
+ which clang++-3.6
+ which gcc
+ which g++-4.9
+ ls /usr/bin/clang*
+ sudo update-alternatives --set gcc /usr/bin/gcc-4.9
+ sudo update-alternatives --set g++ /usr/bin/g++-4.9
+ sudo update-alternatives --set clang /usr/bin/clang-3.6
+ sudo update-alternatives --set clang++ /usr/bin/clang++-3.6
  sudo apt-get install libboost1.54-dev libboost-filesystem1.54-dev libboost-iostreams1.54-dev libboost-system1.54-dev
  sudo apt-get install libx11-dev # libboost-all-dev
  cmake --version
