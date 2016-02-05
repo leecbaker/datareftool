80f976376e5e51f13e2c4c969ae3c01f20e611b1
Lee Baker <lee@leecbaker.com>
2016-02-05 16:06:50 -0700

    TravisCI: Compile linux boost with fPIC

--- build_latest_boost.sh
+++ build_latest_boost.sh
@@ -7,7 +7,7 @@ wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.ta
 tar --bzip2 -xf boost_1_60_0.tar.bz2
 cd boost_1_60_0/
 ./bootstrap.sh --with-libraries=iostreams,filesystem,system --prefix=${INSTALL_DIR}
-./b2 link=static threading=multi runtime-link=shared
+./b2 link=static threading=multi runtime-link=shared cxxflags=-fPIC
 ./b2 install --prefix=${INSTALL_DIR}
 
 
