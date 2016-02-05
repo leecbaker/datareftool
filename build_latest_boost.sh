d9fc25155cb5e570b12f6267a508d49210290f0f
Lee Baker <lee@leecbaker.com>
2016-02-05 15:58:55 -0700

    TravisCI: move boost installation directory

--- build_latest_boost.sh
+++ build_latest_boost.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+INSTALL_DIR=~/boost
+
 wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.bz2
 
 tar --bzip2 -xf boost_1_60_0.tar.bz2
 cd boost_1_60_0/
-./bootstrap.sh --with-libraries=iostreams,filesystem,system --prefix=~/boost
+./bootstrap.sh --with-libraries=iostreams,filesystem,system --prefix=${INSTALL_DIR}
 ./b2 link=static threading=multi runtime-link=shared
-./b2 install --prefix=~/boost
+./b2 install --prefix=${INSTALL_DIR}
 
 
