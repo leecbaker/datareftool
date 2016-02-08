81ac0d08b5b2afd6170f88768c6017db48389a6b
Lee Baker <lee@leecbaker.com>
2016-02-08 12:44:30 -0700

    TravisCI: keep trying to force build with gcc-4.9

--- build_latest_boost.sh
+++ build_latest_boost.sh
@@ -6,8 +6,8 @@ wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.ta
 
 tar --bzip2 -xf boost_1_60_0.tar.bz2
 cd boost_1_60_0/
-./bootstrap.sh --with-toolset=gcc-4.9 --with-libraries=iostreams,filesystem,system --prefix=${INSTALL_DIR}
-./b2 link=static threading=multi runtime-link=shared cxxflags=-fPIC
-./b2 install --prefix=${INSTALL_DIR}
+./bootstrap.sh --with-toolset=gcc --with-libraries=iostreams,filesystem,system --prefix=${INSTALL_DIR}
+./b2 toolset=gcc-4.9 link=static threading=multi runtime-link=shared cxxflags=-fPIC
+./b2 toolset=gcc-4.9 install --prefix=${INSTALL_DIR}
 
 
