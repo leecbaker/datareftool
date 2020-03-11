128d72c5bf4002a6eff3581bfbe006b076e349ab
Lee Baker <lee@leecbaker.com>
2020-03-11 14:31:12 +1200

    Fix issue where the first blacklisted dataref is not loaded.

--- src/lib/scan/scan_files.h
+++ src/lib/scan/scan_files.h
@@ -6,10 +6,6 @@
 
 #include <boost/filesystem/path.hpp>
 
-// Functions that scan a single file in the specified format.
-
-std::vector<std::string> loadBlacklistFile(std::ostream & log, const boost::filesystem::path & filename);
-
 // Load a file that lists a dataref at the beginning of each line, optionally with other material after a whitespace
 std::vector<std::string> loadListFile(std::ostream & log, const boost::filesystem::path & filename);
 
