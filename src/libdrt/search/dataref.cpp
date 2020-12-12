bcde7f1b8d1cd277d57c74c6cac74950b4ac8193
Lee Baker <lee@leecbaker.com>
2020-12-11 20:44:33 -0600

    Zero the buffers before reading array datarefs.
    This should fix some weird values datarefs, like the ones for custom lights.

--- src/libdrt/search/dataref.cpp
+++ src/libdrt/search/dataref.cpp
@@ -80,6 +80,7 @@ bool DataRefUpdater::operator()(std::vector<float>&) const {
     int current_array_length = XPLMGetDatavf(dr->ref, nullptr, 0, 0);
     static std::vector<float> scratch_buffer;
     scratch_buffer.resize(current_array_length);
+    std::fill(scratch_buffer.begin(), scratch_buffer.end(), 0);
     int copied = XPLMGetDatavf(dr->ref, scratch_buffer.data(), 0, current_array_length);
     if(copied == current_array_length) {
         size_t new_hash = boost::hash_range(scratch_buffer.cbegin(), scratch_buffer.cend());
@@ -102,6 +103,7 @@ bool DataRefUpdater::operator()(std::vector<int>&) const {
     int current_array_length = XPLMGetDatavi(dr->ref, nullptr, 0, 0);
     static std::vector<int> scratch_buffer;
     scratch_buffer.resize(current_array_length);
+    std::fill(scratch_buffer.begin(), scratch_buffer.end(), 0);
     int copied = XPLMGetDatavi(dr->ref, scratch_buffer.data(), 0, current_array_length);
     if(copied == current_array_length) {
         size_t new_hash = boost::hash_range(scratch_buffer.cbegin(), scratch_buffer.cend());
